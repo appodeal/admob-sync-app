47c943d89752967d11b51ebf5d1c381ba58b8377
Evgeniy Shurmin <eshurmin@gmail.com>
2019-02-22 12:50:34 +0300

    add electron builder

--- webpack/production.ts
+++ webpack/production.ts
@@ -1,10 +1,13 @@
 import webpack from 'webpack';
 import merge from 'webpack-merge';
-import defaults, {SRC_PATH} from './default';
+import defaults, {ROOT_PATH, SRC_PATH} from './default';
 import * as path from 'path';
 import {environment} from './plugins/environment';
+import GenerateJsonPlugin from 'generate-json-webpack-plugin';
 
 let envConfig = require(path.join(SRC_PATH, '../config/production.json'));
+
+let PACKAGE = require(path.join(ROOT_PATH, 'package.json'));
 
 export default (env: webpack.Configuration): webpack.Configuration => {
     return merge(defaults(env), {
@@ -13,6 +16,14 @@ export default (env: webpack.Configuration): webpack.Configuration => {
             environment(env, envConfig, {
                 development: false,
                 index: './index.html'
+            }),
+            new GenerateJsonPlugin('package.json', {
+                name: PACKAGE.name,
+                author: "Appodeal Inc.",
+                main: "main.js",
+                version: PACKAGE.version,
+                description: PACKAGE.description,
+                dependencies: PACKAGE.dependencies || {}
             })
         ]
     })
