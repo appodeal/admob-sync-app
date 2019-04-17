b3412bd3a032eea5f757bdfc4639db628e2891ab
alex-skakun <alex.skakun@me.com>
2019-04-17 13:42:21 +0300

    [AMSA-37] Display app windows in dock on macOS

--- src/lib/ui-windows.ts
+++ src/lib/ui-windows.ts
@@ -1,5 +1,5 @@
 import {AppodealAccount} from 'core/appdeal-api/interfaces/appodeal.account.interface';
-import {BrowserWindow} from 'electron';
+import {app, BrowserWindow} from 'electron';
 import {AppodealAccountState} from 'interfaces/common.interfaces';
 import {getMapItem} from 'lib/core';
 import {openDialogWindow, openWindow} from 'lib/window';
@@ -10,8 +10,13 @@ const OPENED_WINDOWS = new Map<string, BrowserWindow | Promise<BrowserWindow>>()
 export async function openSettingsWindow () {
     return openOrFocus('settings', async () => {
         let window = await openWindow('./settings.html', {
-            fullscreenable: false
-        }, () => window.removeAllListeners());
+            fullscreenable: false,
+            skipTaskbar: false
+        }, () => {
+            app.dock.hide();
+            window.removeAllListeners();
+        });
+        app.dock.show();
         window.on('focus', async event => {
             let topWindow = getMapItem(OPENED_WINDOWS, OPENED_WINDOWS.size - 1);
             if (topWindow) {
@@ -36,6 +41,13 @@ export function openAppodealSignInWindow (account: AppodealAccountState = null):
                 './sign-in.html',
                 {width: 450, height: 270, parent: await OPENED_WINDOWS.get('settings')},
                 window => {
+                    let parent = window.getParentWindow();
+                    app.dock.show();
+                    window.once('closed', () => {
+                        if (!parent) {
+                            app.dock.hide();
+                        }
+                    });
                     window.webContents.send('existingAccount', JSON.stringify(account));
                     res(window);
                 }
@@ -50,7 +62,16 @@ export function openAppodealAccountsWindow (): Promise<void> {
             openDialogWindow(
                 './manage-accounts.html',
                 {width: 450, height: 350, parent: await OPENED_WINDOWS.get('settings')},
-                res
+                window => {
+                    let parent = window.getParentWindow();
+                    app.dock.show();
+                    window.once('closed', () => {
+                        if (!parent) {
+                            app.dock.hide();
+                        }
+                    });
+                    res(window);
+                }
             ).then(resolve, reject);
         }));
     });
