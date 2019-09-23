407905ba13937e2c9c9c97bd7dc6dab6f47f308b
Alexey Nalivaiko <a.nalivaiko@appodeal.com>
2019-09-23 16:48:59 +0300

    [APD-chore] add extra debug info while parsing admob accounts

--- src/lib/oct-decode.ts
+++ src/lib/oct-decode.ts
@@ -2,5 +2,10 @@ export function decodeOctString (sourceStr: string) {
     if (sourceStr.includes('\'')) {
         throw new Error('invalid Source');
     }
-    return sourceStr.replace(/\\x([A-Za-z0-9]{2})/g, (r, v) => String.fromCharCode(parseInt(v, 16)));
+    try {
+        return (new Function('', `return '${sourceStr}'`))();
+    } catch (e) {
+        console.log(e);
+        return sourceStr.replace(/\\x([A-Za-z0-9]{2})/g, (r, v) => String.fromCharCode(parseInt(v, 16)));
+    }
 }
