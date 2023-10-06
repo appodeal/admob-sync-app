3666f898ecbdb9d47cabfd76e3fb47a01f6ca24b
maksimroi <maksim.roi@appodeal.com>
2023-10-06 19:01:34 +0300

    [AMSA-xxx] Update webpack config

--- webpack/default.ts
+++ webpack/default.ts
@@ -110,7 +110,7 @@ export const entriesConfig = entries.map(
                 ]
             },
             resolve: {
-                extensions: ['.tsx', '.json', '.ts', '.js', '.svg', '.mjs', '.gql', '.graphql'],
+                extensions: ['.tsx', '.css', '.scss', '.json', '.ts', '.js', '.svg', '.mjs', '.gql', '.graphql'],
                 modules: [
                     'node_modules',
                     '@typings',
